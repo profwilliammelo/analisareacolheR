299f0a2ed121c4d65078c9e5288d029b35db4008
profwilliammelo <williamcorrea95@gmail.com>
2023-01-11 13:56:07 -0300

    funcoes iniciais

---
+++ R/funcoes.R
@@ -0,0 +1,10 @@
+#' Boas vindas
+#'
+#' Nao recebe argumentos.
+#'
+#' @param x nao deve ser preenchido, funcao deve ser rodada sem nenhum argumento.
+#'
+#' @export
+ola <- function(x) {print("Olá, você é um estudante ou ex-estudante da Analisar e AcolheR!")
+
+}
